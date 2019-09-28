6e84f15c8a1d402654b3a689f9d665b30560868f
Bhaskara Rao Gummadi <Bhaskar06@yahoo.com>
2019-09-28 07:21:49 +0400

    Updated Variable Names

--- cachematrix.R
+++ cachematrix.R
@@ -3,14 +3,14 @@
 
 ## Function to cache its inverse
 
-makeCacheMatrix <- function(m = matrix()) {
+makeCacheMatrix <- function(x = matrix()) {
 
   matrixinv <- NULL
   set <- function(n){
-    m <<- n
+    x <<- n
     matrixinv <<- NULL
   }
-  get <- function() m
+  get <- function() x
   setInv <- function(inverse) matrixinv <<- inverse
   getInv <- function() matrixinv
   list(set = set, get = get, setInv = setInv,getInv = getInv)
@@ -19,15 +19,15 @@ makeCacheMatrix <- function(m = matrix()) {
 
 ## Function to Get inverse matrix
 
-cacheSolve <- function(m, ...) {
+cacheSolve <- function(x, ...) {
        
-  inv <- m$getInv()
+  inv <- x$getInv()
   if (!is.null(inv)) {
     message("getting cached data")
     return(inv)
   }
-  mat <- m$get()
+  mat <- x$get()
   inv <- solve(mat, ...)
-  m$setInv(inv)
+  x$setInv(inv)
   inv
 }
